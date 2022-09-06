7b373cced15c76869a9e4f2b3ba559d5d25255ca
minor94 <t_has@mail.ru>
2022-09-07 00:50:26 +0300

    added some files

---
+++ main.py
@@ -0,0 +1,102 @@
+# Примеры(строки)
+
+# 2-1
+# message = "Hallo Python world!"
+
+# 2-2
+# message = "need cold day!"
+
+# 2-3
+# name_1 = "Hasan,"
+# print("Hello" , name_1 , "you are can sleep when Aslan say 'Yes, of course you can sleep!'")
+
+# 2-4
+# name = ('aslan teunov')
+# print(name.title())
+# name = ('aslan teunov')
+# print(name.upper)
+# name = ("Aslan Teunov")
+# print(name.lower)
+
+# 2-5
+# print('Aslan Teunov часто думает"Больше котиков я люблю еще больше котиков!"')
+
+# 2-6
+# name = 'Aslan Teunov'
+# message = ('часто думает"Больше котиков я люблю еще больше котиков!"')
+
+# 2-7
+# print(name , message )
+# name = (' \tAslan \n\tTeunov ') 
+# name = ( "Aslan Teunov" )
+# print(name.lstrip())
+# print(name.rstrip())
+# Числа
+# 2-8
+# print(10-2)
+# print(2*4)
+# print(32/4)
+# print(5+3)
+
+# 2-9
+# love_namber = 20
+# print(love_namber)
+# print('Мое любимое число это' , love_namber)
+
+# 3-1
+# names = ['aslan', 'sofi', 'archi', 'hasan']
+# print(names[0].title())
+# print(names[-1].upper())
+
+# 3-2
+# famalys = ['aslan', 'archi', 'sofi', 'hasan']
+# print('Моя единственный родной брат' , famalys[0].title() + '!.')
+# print('Моя любимая и неповторимая' , famalys[2].title() + '!.')
+# print('Второй собственник квартиры это' , famalys[1].title() + '!.')
+# print(famalys[0].title() ,'обучает меня програмированнию.А меня если что звать' , famalys[-1].title())
+
+# 3-3
+# cars = ['bmv', 'audi', 'kia', 'mersedes']
+# print('Я люблю все марки автомобилей но мне подуше' , cars[2].title() + ',' 'особенно "Carnival"' + '!.')
+# 3-4 Доучить!!!!
+# quests = ['Aslan', 'Sofi', 'Archi', 'Hasan']
+# del quests[2]
+# quests.insert(0, "Aslanchik")
+# quests.insert(2, 'Archi_two')
+# quests.insert(-1, "Hasanchik")
+# quests.append('Spoon')
+# print('Добро пожаловать на ланч' , questse , '!.')
+
+# 3-8
+# countries = ('japan', 'turke', 'france', 'vinec')
+# print(countries)
+# print(sorted(countries))
+# print(countries)
+# print(countries)
+# countries.reverse()
+# print(countries)
+
+# 3-9
+# visits = ['Aslan', 'Hasan', 'Sofi', 'archi']
+# print(visits)
+# len(visits)
+
+# 4-1
+# users = ['aslan', 'sofi', 'archi', 'hasan']
+# for user in users:
+# print(user.title() + "," 'спасибо что пользуйтесь нашим сервисом!' , '\n')
+
+# print('Спасибо что пользуйтесь нашими услугами!')
+# home_pets = ['кошка', 'собака', 'сова', 'крыса', 'енот']
+# for pet in home_pets:
+# print(pet.title() , '"мы тоже любим своих хозяев!"')
+
+# 4-3
+# for namber in range(1,21):
+#   print(namber)
+
+# from unicodedata import name
+
+
+name = "asdsad"
+print(name)
